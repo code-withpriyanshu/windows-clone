a6d38cfc919b0e8cf9d2e13a44bcd6fc10a8bbf8
Priyanshu Patel <139453653+code-withpriyanshu@users.noreply.github.com>
2024-06-19 00:28:28 +0530

    done for today

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 function baby() {
   console.log("heelo");
 }
+
+// minimenu
+let bg = document.getElementById("wallpaper");
+let minimenu = document.getElementById("minimenu");
+bg.addEventListener("contextmenu", (e) => {
+  console.log(e);
+  minimenu.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+  minimenu.style.visibility = "visible";
+});
+bg.addEventListener("dblclick", (e) => {
+  console.log(e);
+  minimenu.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+  minimenu.style.visibility = "hidden";
+});
+bg.addEventListener("click", (e) => {
+  folder.style.backgroundColor = "null";
+});
+
+// folder
+let folders = document.querySelectorAll(".folder");
+folders.forEach((folder) => {
+  folder.addEventListener("click", () => {
+    folder.style.backgroundColor = "blue";
+  });
+});
